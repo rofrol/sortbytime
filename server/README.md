d84190a3d80b90c3ad597cbc590f6f42133c7773
Roman Frołow <rofrol@gmail.com>
2019-06-08 12:05:41 +0200

    Second commit

---
+++ server/README.md
@@ -0,0 +1,38 @@
+## html exploration
+
+`cargo install htmlq`
+
+count: `htmlq -p "#itemsStream > li .diggbox span:first-of-type" < out.html`
+
+title: `htmlq -p "#itemsStream > li:first-of-type h2 a" --text < out.html`
+
+date_published: `htmlq -p '#itemsStream > li:first-of-type [itemprop="datePublished"]' --attribute datetime < out.html`
+
+source: `htmlq -p '#itemsStream > li:first-of-type [title~="źródło"]' --attribute href  < out.html`
+
+description: `htmlq -p '#itemsStream > li:first-of-type .description' --text < out.html`
+
+## test request
+
+`curl -H "Content-Type: application/json" -X POST "http://localhost:8080/hity" -d '{"id":"1", "name": "JohnDoe"}'`
+
+## Run tests
+
+Run all tests except ignored:
+
+`cargo test`
+
+Or single test
+
+`cargo test get_items_test`
+
+Run ignored tests
+
+`cargo test -- --ignored`
+
+To show println!
+
+```bash
+$ cargo test -- --nocapture
+$ cargo test get_items_test -- --nocapture
+```
--- server/src/main.rs
+++ server/src/main.rs
@@ -18,14 +18,22 @@ extern crate validator_derive;
 #[macro_use]
 extern crate serde_derive;
 
-use std::collections::HashMap;
 use std::io;
 
 use actix_web::client::Client;
-use actix_web::web::BytesMut;
-use actix_web::{web, App, Error, HttpResponse, HttpServer};
-use futures::{Future, Stream};
+use actix_web::{web, App, Error as ActixError, HttpResponse, HttpServer};
+use futures::Future;
 use validator::Validate;
+
+extern crate kuchiki;
+use kuchiki::traits::*;
+use kuchiki::NodeRef;
+//use kuchiki::tree::{ElementData, Html, NodeDataRef, NodeRef};
+
+use derive_more::Display;
+
+extern crate chrono;
+use chrono::{DateTime, Local};
 
 #[derive(Debug, Validate, Deserialize, Serialize)]
 struct SomeData {
@@ -35,43 +43,161 @@ struct SomeData {
     name: String,
 }
 
-#[derive(Debug, Deserialize)]
-struct HttpBinResponse {
-    args: HashMap<String, String>,
-    data: String,
-    files: HashMap<String, String>,
-    form: HashMap<String, String>,
-    headers: HashMap<String, String>,
-    json: SomeData,
-    origin: String,
-    url: String,
+fn hity(
+    _some_data: web::Json<SomeData>,
+    client: web::Data<Client>,
+) -> impl Future<Item = HttpResponse, Error = ActixError> {
+    client
+        .get("https://www.wykop.pl/hity/dnia/")
+        .send()
+        .map_err(ActixError::from)
+        .and_then(|mut resp| {
+            resp.body().from_err().and_then(|body| {
+                let body_string = String::from_utf8(body.to_vec()).unwrap();
+                let items = get_items(&body_string);
+                println!("items: {:?}", items);
+
+                Ok(HttpResponse::Ok().body("Hello"))
+            })
+        })
 }
 
-// curl -H "Content-Type: application/json" -X POST "http://localhost:8080/hity" -d '{"id":"1", "name": "JohnDoe"}'
-fn hity(
-    some_data: web::Json<SomeData>,
-    client: web::Data<Client>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    client
-        .post("https://httpbin.org/post")
-        .send_json(&some_data.into_inner())
-        .map_err(Error::from) // <- convert SendRequestError to an Error
-        .and_then(|resp| {
-            resp.from_err()
-                .fold(BytesMut::new(), |mut acc, chunk| {
-                    acc.extend_from_slice(&chunk);
-                    Ok::<_, Error>(acc)
+#[derive(Debug, PartialEq)]
+struct Item {
+    count: u32,
+    title: String,
+    href: String,
+    date_published: DateTime<Local>,
+    source: String,
+    description: String,
+    author: String,
+    author_url: String,
+}
+
+#[derive(Debug, Display)]
+pub enum MyError {
+    #[display(fmt = "Missing item")]
+    MissingItems,
+}
+
+fn get_items(html: &str) -> Result<Vec<Item>, MyError> {
+    let css_selector = "#itemsStream li";
+
+    // println!("{:?}", html);
+    let document = kuchiki::parse_html().one(html);
+    Ok(document
+        .select(css_selector)
+        .map_err(|()| MyError::MissingItems)?
+        .take(1)
+        .map(|item| {
+            println!("{:?}", item);
+
+            println!("{:?}", item.text_contents());
+
+            let item_node: &kuchiki::NodeRef = item.as_node();
+
+            let count_node_data_ref = item_node
+                .select_first(".diggbox span:first-of-type")
+                .unwrap();
+            println!("count_node_data_ref: {:?}", count_node_data_ref);
+            println!(
+                "count_node_data_ref.text_contents(): {:?}",
+                count_node_data_ref.text_contents()
+            );
+            let count: u32 = count_node_data_ref.text_contents().parse().unwrap();
+
+            let a_node_data_ref = item_node.select_first("h2 a").unwrap();
+            let a_node = a_node_data_ref.as_node();
+
+            let title: String = a_node_data_ref.text_contents();
+            println!("title: {}", title);
+
+            let a_elt = a_node.as_element().unwrap();
+            let href = a_elt.attributes.borrow().get("href").unwrap().to_string();
+            println!("href: {:?}", href);
+
+            let date_published_string = item_node
+                .select_first("[itemprop='datePublished']")
+                .unwrap()
+                .as_node()
+                .as_element()
+                .unwrap()
+                .attributes
+                .borrow()
+                .get("datetime")
+                .unwrap()
+                .to_string();
+
+            let date_published = DateTime::parse_from_rfc3339(&date_published_string)
+                .unwrap()
+                .with_timezone(&Local);
+            println!("date_published: {:?}", date_published);
+
+            let source = item_node
+                .select_first("[title~='źródło']")
+                .unwrap()
+                .as_node()
+                .as_element()
+                .unwrap()
+                .attributes
+                .borrow()
+                .get("href")
+                .unwrap()
+                .to_string();
+
+            println!("source: {:?}", source);
+
+            let description = item_node
+                .select_first(".description a")
+                .unwrap()
+                .as_node()
+                .text_contents()
+                .trim()
+                .to_string();
+
+            println!("description: {:?}", description);
+
+            let author_node_data_ref = item_node.select_first("a[href*='ludzie'").unwrap();
+            let author_node = author_node_data_ref.as_node();
+            println!("author_node: {:?}", author_node);
+
+            let author = author_node.text_contents().trim().to_string();
+
+            println!("author: {:?}", author);
+
+            let author_url = author_node
+                .as_element()
+                .unwrap()
+                .attributes
+                .borrow()
+                .get("href")
+                .unwrap()
+                .to_string();
+
+            println!("author_url: {:?}", author_url);
+
+            let author_nodes: Vec<NodeRef> = author_node
+                .descendants()
+                .filter(|node| {
+                    println!("node: {:?}", node);
+                    true
                 })
-                .map(|body| {
-                    let body: HttpBinResponse = serde_json::from_slice(&body).unwrap();
-                    body.json
-                })
+                .collect();
+
+            println!("author_nodes: {:?}", author_nodes);
+
+            Item {
+                count: count,
+                title: title,
+                href: href,
+                date_published: date_published,
+                source: source,
+                description: description,
+                author: author,
+                author_url: author_url,
+            }
         })
-        .and_then(|d| {
-            Ok(HttpResponse::Ok()
-                .content_type("application/json")
-                .body(serde_json::to_string(&d).unwrap()))
-        })
+        .collect())
 }
 
 fn main() -> io::Result<()> {
@@ -87,4 +213,33 @@ fn main() -> io::Result<()> {
     })
     .bind(endpoint)?
     .run()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_items_test() -> Result<(), Box<std::error::Error>> {
+        use std::fs;
+        let html = fs::read_to_string("out.html").expect("Unable to read file");
+        let actual = get_items(&html).unwrap();
+
+        let expected: Vec<Item> = vec![
+            Item {
+                count: 2754,
+                title: "Przejazd straży pożarnej w trakcie biegu Piotrkowska Rossman".to_string(),
+                href: "https://www.wykop.pl/link/4971063/przejazd-strazy-pozarnej-w-trakcie-biegu-piotrkowska-rossman/".to_string(),
+                date_published: DateTime::parse_from_rfc3339("2019-05-26T12:26:01+02:00").unwrap().with_timezone(&Local),
+                source: "https://m.youtube.com/watch?v=74eMjNg_dmE&feature=youtu.be".to_string(),
+                description: "Czy bieganie jest ważniejsze od ratowania życia?".to_string(),
+                author: "@Bananowy96".to_string(),
+                author_url: "https://www.wykop.pl/ludzie/Bananowy96/".to_string(),
+            }
+        ];
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
 }
